84afac4ce2755d95d559f90b5e88f58479c6a9a6
Chris Paul <34114230+cpaul319@users.noreply.github.com>
2019-02-13 00:58:56 -0500

    manual update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,52 +7,54 @@ var guessLetterArr =[];
 
 
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+document.getElementById("gameInstructions").innerHTML= "Click anywhere to start game";
+  
 
-        var randomNumber = Math.floor(Math.random() *letters.length);
-        var randomLetter = letters[randomNumber];
-        console.log(randomLetter);
+      
+       
         // var chosenLetter = letters[randomLetter];
         document.onkeyup = function(event) {
         guess = event.key.toUpperCase();
         console.log(guess);
-        for (var i =0; i< 10; i++) {
-            guessLetterArr.push(guess);
+        
+           
              if (guess === randomLetter){
-                 i = 10;
+                wins++;
+                document.getElementById("wins").innerHTML= wins;
+                alert("Congrats, you picked the right letter");
              }
-            alert("Congrats, you are right");
-            wins++;
-            document.getElementById("wins").innerHTML= wins;
-            } else {
-                 
-                alert("Guess again");
-                guessLetterCount--;
-                document.getElementById("gameInstructions").innerHTML= "Pick another letter";
-                document.getElementById("letterGuess").innerHTML=  guessLetterArr;
-                guessLetterCount--;
-                document.getElementById("letterCount").innerHTML=  guessLetterCount;
-                document.getElementById("losses").innerHTML=  losses;
-
-                    }
-                    }
-
-                    $("#first-number, #second-number, #operator, #result").empty();{}
-                }
-          
-          
-                
+            else if (guess !== randomLetter){
+             guessLetterArr.push(guess);
+             
+             guessLetterCount--;
+             document.getElementById("gameInstructions").innerHTML= "Pick another letter";
             
+             
+             document.getElementById("letterGuess").innerHTML=  guessLetterArr;
+            
+             document.getElementById("letterCount").innerHTML=  guessLetterCount;
+             document.getElementById("losses").innerHTML=  losses;
+             }
+                
+ 
+                            if (guessLetterCount == 0) {
+                            guessLetterCount = 10;
+                            guessLetterArr =[];
+                         
+                            losses++;
+                            document.getElementById("myBtn").addEventListener("click", function() {
+                                var randomNumber = Math.floor(Math.random() *letters.length);
+                                var randomLetter = letters[randomNumber];
+                                alert("New Game");
+                              
+                                 console.log(randomLetter);
+                              });
+                            
+                            document.getElementById("letterGuess").innerHTML = " ";
+                            document.getElementById("losses").innerHTML = " ";
+                            document.getElementById("LetterGuess").innerHTML = guessLetterCount;
 
-   //game reset
-   if (guessLetterCount == 0) {
-    guessLetterCount = 10;
-    losses++;
-    
-    document.getElementById("letterGuess").textContent = " ";
-    document.getElementById("losses").textContent = " ";
-    document.getElementById("LetterGuess").textContent = "10";
-    randomLetter = computerGuess();
-    // psychicGame();
+                        }
 
                                 }
-                                                     
+                                    
