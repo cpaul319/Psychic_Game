1beb526830c9a8517429b1b47146921100423535
Chris Paul <34114230+cpaul319@users.noreply.github.com>
2019-02-13 21:32:32 -0500

    manual commit working version 1.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,55 +6,67 @@ var guessLetterArr =[];
 
 
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-document.getElementById("gameInstructions").innerHTML= "Click anywhere to start game";
-  
 
+
+
+function resetGame(){
+   guessLetterArr =[];
+   document.getElementById("LetterGuess").innerHTML = guessLetterCount;
+   document.getElementById("losses").innerHTML=  losses;
+   document.getElementById("letterGuess").innerHTML=  guessLetterArr;
+   randomLetter = getRandomLetter();
+
+}
+
+
+function getRandomLetter(){
+   var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var randomNumber = Math.floor(Math.random() *letters.length);
+var randomLetter = letters[randomNumber];
+return randomLetter;
+
+}
+
+function lostGame(){
+   alert("I'm sorry. You are out of guesses. You lose.");
+   losses++;
+   document.getElementById("losses").innerHTML=  losses;
+   alert("Want to try again");
+   guessLetterCount = 10;
+   resetGame();
+}
+
+
+   
+   document.onkeyup = function(event) {
+      document.getElementById("gameInstructions").innerHTML= "Click anywhere to start game";
+      guess = event.key.toUpperCase();
+      console.log(guess);
+    randomLetter=getRandomLetter();
+   
       
-       
-        // var chosenLetter = letters[randomLetter];
-        document.onkeyup = function(event) {
-        guess = event.key.toUpperCase();
-        console.log(guess);
-        
-           
-             if (guess === randomLetter){
-                wins++;
-                document.getElementById("wins").innerHTML= wins;
-                alert("Congrats, you picked the right letter");
-             }
-            else if (guess !== randomLetter){
-             guessLetterArr.push(guess);
-             
-             guessLetterCount--;
-             document.getElementById("gameInstructions").innerHTML= "Pick another letter";
-            
-             
-             document.getElementById("letterGuess").innerHTML=  guessLetterArr;
-            
-             document.getElementById("letterCount").innerHTML=  guessLetterCount;
-             document.getElementById("losses").innerHTML=  losses;
-             }
-                
+   if (guess === randomLetter){
+               
+      wins++;
+      document.getElementById("wins").innerHTML= wins;
+      alert("Congrats, you picked the right letter. You win");
+      resetGame();
+   }
+  else {
+   guessLetterArr.push(guess);
+   guessLetterCount--;
+   document.getElementById("gameInstructions").innerHTML= "Pick another letter";
+   document.getElementById("letterGuess").innerHTML=  guessLetterArr;
+   document.getElementById("letterCount").innerHTML=  guessLetterCount;
+   }
+      
+   if (guessLetterCount === 0) {
+   lostGame();
+   
+   }
+   
+
+
  
-                            if (guessLetterCount == 0) {
-                            guessLetterCount = 10;
-                            guessLetterArr =[];
-                         
-                            losses++;
-                            document.getElementById("myBtn").addEventListener("click", function() {
-                                var randomNumber = Math.floor(Math.random() *letters.length);
-                                var randomLetter = letters[randomNumber];
-                                alert("New Game");
-                              
-                                 console.log(randomLetter);
-                              });
-                            
-                            document.getElementById("letterGuess").innerHTML = " ";
-                            document.getElementById("losses").innerHTML = " ";
-                            document.getElementById("LetterGuess").innerHTML = guessLetterCount;
-
-                        }
-
-                                }
-                                    
+} 
